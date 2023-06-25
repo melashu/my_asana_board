84839359d18bb922e06f5c529a123289306fa916
melashu <meshu102@outlook.com>
2023-06-25 08:48:46 +0300

    Add all controller actions

---
+++ app/controllers/homes_controller.rb
@@ -0,0 +1,49 @@
+require 'json'
+class HomesController < ApplicationController
+  include ApiForAsana
+  def index
+    @code = get_sections.code
+    @body = JSON.parse(get_sections.body)
+  end
+  
+  def edit
+    @id = params[:id]
+  end
+
+    def new
+    end
+
+    def create
+        name = params[:name]
+        res = create_section(name)
+        if res.code == '201'
+          redirect_to get_sections_path, notice: 'Successfuly create section!'
+        else
+          data = JSON.parse(res.body)
+          redirect_to get_sections_path, alert: "#{data["errors"][0]["message"]}"
+        end
+    end
+
+    def destroy
+      id = params[:id]
+      res = delete_section(id)
+      if res.code == '200'
+        redirect_to get_sections_path, notice: 'Successfuly delete section!'
+      else
+        data = JSON.parse(res.body)
+        redirect_to get_sections_path, alert: "Error happened #{data["errors"][0]["message"]}"
+      end
+    end
+
+
+    def update
+      id = params[:id]
+      name = params[:name]
+      res = update_section(id, name)
+      if res.code == '200'
+        redirect_to get_sections_path, notice: "Successfuly update your section to #{name}"
+      else
+        redirect_to get_sections_path, alert: "#{res.body["errors"][0]["message"]}"
+      end
+    end 
+end
